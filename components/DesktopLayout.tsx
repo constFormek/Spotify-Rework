94b4f9de6de6019b83b3626413b338eaffeed93c
plexi <plexify.code@gmail.com>
2026-07-18 15:51:22 +0200

    feat: split library tab from tabs, render tabs as subgrid

--- components/DesktopLayout.tsx
+++ components/DesktopLayout.tsx
@@ -5,35 +5,30 @@ import Link from "next/link";
 
 type TabType = {
   label: string;
-  icon: IconName;
   href: string;
-  id: number;
+  icon: IconName;
 };
 
 export const DesktopLayoutTabs: TabType[] = [
   {
-    id: 1,
     label: "My Library",
     href: "/library",
     icon: "library",
   },
 
   {
-    id: 2,
     label: "Home",
     href: "/",
     icon: "home",
   },
 
   {
-    id: 3,
     label: "Discover",
     href: "/discover",
     icon: "discover",
   },
 
   {
-    id: 4,
     label: "Search",
     href: "/search",
     icon: "search",
@@ -45,20 +40,36 @@ interface DesktopLayoutProps {
 }
 
 const DesktopLayout = ({ children }: DesktopLayoutProps) => {
+  const libraryTab = DesktopLayoutTabs.find((t) => t.href === "/library");
+  const navbarTabs = DesktopLayoutTabs.filter((t) => t.href !== "/library");
+
   return (
     <div className="hidden h-screen md:grid grid-cols-[1fr_4fr] grid-rows-[auto_1fr_auto]   gap-y-1 gap-x-2 px-2 py-1 ">
-      <div className="border border-bg-secondary rounded-lg col-span-2 flex items-center gap-2">
-        {DesktopLayoutTabs.map((tab) => (
+      <div className="border border-bg-secondary rounded-lg col-span-2 grid  items-center gap-2 grid-cols-subgrid">
+        {libraryTab && (
           <Link
-            href={tab.href}
-            key={tab.id}
+            href={libraryTab.href}
             className="flex items-center gap-2 text-fg-secondary pl-3.75"
           >
-            <Icon name={tab.icon} size={40} />
+            <Icon name={libraryTab.icon} size={40} />
 
-            {tab.label}
+            {libraryTab.label}
           </Link>
-        ))}
+        )}
+
+        <div className="flex items-center gap-2 ">
+          {navbarTabs.map((tab) => (
+            <Link
+              href={tab.href}
+              key={tab.href}
+              className="flex items-center gap-2 text-fg-secondary pl-3.75"
+            >
+              <Icon name={tab.icon} size={40} />
+
+              {tab.label}
+            </Link>
+          ))}
+        </div>
       </div>
 
       <div className="border border-bg-secondary rounded-lg row-span-2">
